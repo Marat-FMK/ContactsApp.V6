55a0c94036c3e8cc295d4ced01cbfc0d7ef4f4d5
MaratXcode <kosjachok8@yandex.ru>
2024-06-30 21:29:29 +0300

    Initial Commit

---
+++ ContactsApp.V6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp.V6
+//
+//  Created by Marat Fakhrizhanov on 30.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
